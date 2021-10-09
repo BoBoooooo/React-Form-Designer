ad48850ed04ef9ba524bab2d2140c6e2fa3a55af
BoBoooooo <17746714@qq.com>
2021-10-09 15:05:04 +0800

    fix: ts bug

--- packages/form-designer/src/pages/Layout.tsx
+++ packages/form-designer/src/pages/Layout.tsx
@@ -300,7 +300,7 @@ const App = () => {
                     } else {
                       callback();
                     }
-                  } catch (err) {
+                  } catch (err: any) {
                     callback(err);
                   }
                 },
